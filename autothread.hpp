e5ad5ddf19eb30b31222265506bca88236afadee
steel99xl <steel99xl@gmail.com>
2023-01-24 17:44:56 -0500

    fixed formating

--- autothread.hpp
+++ autothread.hpp
@@ -1,96 +1,80 @@
 #pragma once
+#include <chrono>
+#include <functional>
 #include <iostream>
 #include <thread>
-#include <functional>
-#include <chrono>
 
-namespace autothread{
+namespace autothread {
 
+enum States { START, STOP, WAIT, BOOLWAIT };
 
-enum States {START, STOP, WAIT, BOOLWAIT};
+struct Thread {
 
+  // BOOL = 0 = thread.RUN
 
+  bool AutoLoop = false; // If set to true your function thread will run on a
+                         // loop untill told to stop
 
+  States State = START;
 
+  int WaitTime = 0; // Time stored in milliseconds
 
-struct Thread{
+  //	virtual void Input();
 
-	// BOOL = 0 = thread.RUN
-	
-	bool AutoLoop = false; //If set to true your function thread will run on a loop untill told to stop
+  virtual void FunctionCallBack() { std::cout << "PLS OVERIDE" << std::endl; };
 
-	States  State = START;
-
-	int WaitTime = 0; //Time stored in milliseconds
-
-//	virtual void Input();
-
-	virtual void FunctionCallBack(){std::cout << "PLS OVERIDE" << std::endl;};
-
-	
-
-	inline	void Manager(States S, int T = 0){
-		this->State = S;
-		this->WaitTime = T;
-
-	};
-
-
+  inline void Manager(States S, int T = 0) {
+    this->State = S;
+    this->WaitTime = T;
+  };
 };
 
+inline void Machine(Thread *T) {
+  int tmp = T->WaitTime;
 
-inline	void Machine(Thread *T){
-		int tmp = T->WaitTime;
+  do {
+    switch (T->State) {
 
-		do {	
-			switch(T->State){
-				
-				case STOP:{
+    case STOP: {
 
-					// TO STOP
-					T->AutoLoop = false;	
-				} break;
+      // TO STOP
+      T->AutoLoop = false;
+    } break;
 
-				case WAIT:{	
-							  auto time = std::chrono::milliseconds(tmp);
-							  std::this_thread::sleep_for(time);
-							  // This sets the local tmp  value without effecting the wait time if wanted for auto loop
-							  tmp = 0;
+    case WAIT: {
+      auto time = std::chrono::milliseconds(tmp);
+      std::this_thread::sleep_for(time);
+      // This sets the local tmp  value without effecting the wait time if
+      // wanted for auto loop
+      tmp = 0;
 
-							  // After this is passes over BOOLWAIT to START
-					
-				}
+      // After this is passes over BOOLWAIT to START
+    }
 
-				case BOOLWAIT:{
-								  if(T->WaitTime == 0){
-									 //Nothing to do besied move to START
-								  } else if (T->WaitTime == 1) {
-									  std::this_thread::sleep_for(std::chrono::milliseconds(1));
-									 continue;
-								  } else{
-									  T->AutoLoop = STOP;
-									  std::cout << "ERROR STOPPING THREAD" << std::endl;
-									  break;
-								  }
+    case BOOLWAIT: {
+      if (T->WaitTime == 0) {
+        // Nothing to do besied move to START
+      } else if (T->WaitTime == 1) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        continue;
+      } else {
+        T->AutoLoop = STOP;
+        std::cout << "ERROR STOPPING THREAD" << std::endl;
+        break;
+      }
+    }
 
-							}
+    case START: {
+      T->FunctionCallBack();
+    } break;
+    };
+  } while (T->AutoLoop);
+};
 
-				case START:{
-							   T->FunctionCallBack();
-						   } break;
+inline void StartWork(Thread *T) {
 
-
-			};
-		} while(T->AutoLoop);
-		
-	};
-
-
-inline void StartWork(Thread *T){
-
-	std::thread Worker(Machine, T);
-	Worker.detach();
+  std::thread Worker(Machine, T);
+  Worker.detach();
 }
 
-
-};
+}; // namespace autothread
